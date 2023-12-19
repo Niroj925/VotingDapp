10bdf3ad6038f3ecd098c6cabeb4adcfb0e5091a
Niroj Thapa <ngtc0925@gmail.com>
2023-12-19 22:23:36 +0545

    changes

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -5,7 +5,7 @@ async function main() {
   const votingContract=await hre.ethers.getContractFactory("Voting");
   const deployedVotingContract=await votingContract.deploy();
 
-  console.log(`contract address deployed: ${deployedVotingContract.target}`);
+  console.log(`deployed contract address: ${deployedVotingContract.target}`);
 }
 
 main().catch((error) => {
